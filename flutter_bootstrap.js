52e529e6c4ca7d3db4a586fddde578a1d20ac164
rutujap1604 <rutujapatil16dec@gmail.com>
2026-04-13 17:39:21 +0000

    Deploying to gh-pages from @ rutujap1604/Mock-CET_LiveDemo@52f879d3209ba227ae8a3752d46461584b7a4a93 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1876774934" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2313867670" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
